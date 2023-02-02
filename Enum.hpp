0049b774bf29a8e4b399ad860ae6b145fbb9162f
Arthur MACDONALD <arthur.macdonald@etu.u-bordeaux.fr>
2023-02-02 16:16:25 +0100

    Modified folder structure

--- Enum.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef ENUM_STRUCT
-#define ENUM_STRUCT
-
-enum Direction {UP, DOWN, RIGHT, LEFT};
-enum Animation {STANDING_ANIMATION, SPIN_ANIMATION, WALK_ANIMATION};
-
-#endif
--- Map.cpp
+++
@@ -1,9 +0,0 @@
-#include "Map.hpp"
-
-
-Map::Map(int x, int y, int width, int height, int img_width, int img_height, char* image_path, SDL_Renderer* renderer) : GameObject(x,y,width,height,img_width,img_height, 0, DOWN,image_path,renderer) {
-
-};
-
-Map::~Map() {
-};
